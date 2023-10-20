8566c46bf30a03ab0077e47fc881457ebbe4d5d1
nikiforovaE <121877546+nikiforovaE@users.noreply.github.com>
2023-10-21 01:33:26 +0300

    Add files via upload

---
+++ Lab 2/task1.py
@@ -0,0 +1,15 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+count_months = 0
+
+while True:
+    money_capital += (salary - spend)
+    spend += spend * increase
+    if money_capital > 0:
+        count_months += 1
+    else:
+        break
+print("Количество месяцев, которое можно протянуть без долгов:", count_months)
